f14e70a92bf2e7232afea4f826ff14ac9fe3b568
Shrijan Kumar <shri172004@gmail.com>
2024-05-12 11:41:40 +0530

    created a function for human to choose b/w rock, paper, and scissor

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 console.log("hello world");
 
-function getCompChoice(){
-   let x = Math.floor(Math.random() * (3 - 0))
-   console.log(x)
-   if (x===0){
-    console.log("Rock");
-   }
-   else if(x===1){
-    console.log("Paper");
-   }
-   else{
-    console.log("Scissor");
-   }
+function getCompChoice() {
+    let x = Math.floor(Math.random() * (3 - 0))
+    console.log(x)
+    if (x === 0) {
+        console.log("Rock");
+    }
+    else if (x === 1) {
+        console.log("Paper");
+    }
+    else {
+        console.log("Scissor");
+    }
 }
-
 getCompChoice()
+
+function getHumanChoice() {
+    let a = prompt(`Enter a value between 0 to 2
+    0 means Rock
+    1 means Paper
+    2 means Scissor`
+    )
+    if (a === 0) {
+        console.log("Rock")
+    }
+    else if (a === 1) {
+        console.log("Paper");
+    }
+    else {
+        console.log("Scissor");
+    }
+}
+getHumanChoice()
